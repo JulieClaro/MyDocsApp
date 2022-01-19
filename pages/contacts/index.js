51a6e121e4b7a42467481550d08034f47908f4a3
Julie Claro de Oliveira <94471326+FbSilva13@users.noreply.github.com>
2022-01-19 13:42:35 -0300

    mydocsapp4

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -1,10 +1,10 @@
 /** ./pages/contacts/index.js
  * 
- * Este JavaScript é de uso exclusivo da página/rota 'contacts'.
- * Ele faz a validação e processa o envio do formulário no front-end.
+ * Este JavaScript é de uso exclusivo da página/rota 'contasts'.
+ * ele faz validação e processa o envio do formulário no front-end.
  * 
- * By Luferat - https://github.com/Luferat
- * MIT License - https://opensource.org/licenses/MIT
+ * By Luferat → http://github.com/Luferat 
+ * MIT Lisence → https://opensource.org/licenses/MIT
  */
 
 // Define a página de reload
@@ -14,130 +14,126 @@ setPage('contacts');
 setTitle('Faça contato');
 
 /**
- * Cria a função de processamento do formulário somente se não existe na 
- * memória.
+ * Cria função de processamento do formulário somente se não existe na memória.
  *   Referências:
  *     https://www.w3schools.com/js/js_typeof.asp
  *     https://www.w3schools.com/jsref/prop_text_value.asp
- *     https://www.w3schools.com/js/js_json_stringfy.asp
+ *     https://www.w3schools.com/js/js_json_stringify.asp
  */
-if (typeof sendForm !== 'function') {
+if (typeof sendForm !== "function") {
 
     console.log('Criando função "sendForm"...');
 
-    // Função que processa o formulario
     window.sendForm = function () {
 
         console.log('Enviando contato...');
 
-        // Obtém os campos do formulário e sanitiza.
+        /**
+         * Obtém e sanitiza os campos preenchidos.
+         * A função 'sanitizeString()' é declarada em 'global.js'.
+         * Campos adicionais podem ser processados aqui. Por exemplo:
+         *   contact.date → Data de envio do contato;
+         *   contact.status → Campo de controle sobre status do contato.
+         */
         var contact = {
-            name: sanitizeString(el('#contactName').value),
-            email: sanitizeString(el('#contactEmail').value),
-            subject: sanitizeString(el('#contactSubject').value),
-            message: sanitizeString(el('#contactMessage').value)
+            name: sanitizeString(el('#contact-name').value),
+            email: sanitizeString(el('#contact-email').value),
+            subject: sanitizeString(el('#contact-subject').value),
+            message: sanitizeString(el('#contact-message').value),
+            date: getSystemDate(),
+            status: 'recebido'
         }
 
         /**
-         * Não envia o formulário se algum campo está vazio.
+         * Não envia o form se algum campo esta vazio.
          * Isso é útil caso a validação do HTML/CSS falhe.
          */
         var empty = false;
         for (let key in contact) {
-            if (contact[key] === '') {
-
-                // Primeira letra de 'key' maiúscula
-                let ucKey = key[0].toUpperCase() + key.substr(1);
-
-                // Reescreve o campo
-                el(`#contact${ucKey}`).value = '';
-
+            if (contact[key] == '') {
+                el(`#contact-${key}`).value = '';
                 empty = true;
-            }
+            };
         }
         if (empty) return false;
-
-        // Adiciona a data de envio e o status do contato
-        contact.date = getSystemDate();
-        contact.status = 'recebido';
 
         /**
          * Salva contato no banco de dados.
          * 
-         * O que vai acontecer aqui, depende de seu back-end e de como ele vai
-         * receber os dados do front-end. Provavelmente uma API REST que recebe
-         * os dados em JSON.
+         * O que vai acontecer aqui depende de seu back-end e de como ele vai 
+         * receber os dados do front-end. Provalmente uma API REST que recebe
+         * os dados em JSON...
          * 
-         * Veja um exemplo à seguir...
-         */
-        console.log('Salvei isso no banco de dados --> ', contact);
-
-        /**
-         * Faz a conexão com a API REST contendo o banco de dados usando o
-         * método HTTP 'POST' e postando os dados no 'body' do documento 
-         * enviado, formatado como um JSON.
-         * 
-         * Neste exemplo, está salvando em um banco de dados JSON 
-         * (./db/db.json) privido pelo 'json-server'.
+         * No exemplo, está salvando em um banco de dados JSON (./db/db.json)
+         * provido pelo 'json-server'.
          *   Referências:
          *     https://github.com/typicode/json-server
          */
-        fetch(`${config.apiURL}contacts`, {
+
+        // console.log(contact); // Dados brutos.
+        // console.log(JSON.stringify(contact)); // Dados em JSON.
+
+        /**
+         * Faz a conexão com a API REST contendo o banco de dados usando o 
+         * método HTTP 'POST' e postando os dados no 'body' do documento enviado, 
+         * formatando-o como um JSON.
+         */
+        fetch('http://localhost:3030/contacts', {
             method: "POST",
             body: JSON.stringify(contact),
             headers: { "Content-type": "application/json; charset=UTF-8" }
         })
 
-            // Resposta do 'fetch' --> Contato enviado.
+            // Resposta do 'fetch'
             .then(response => {
 
                 // Se falhou por algum motivo...
                 if (!response.ok) {
 
-                    // Formata mensagem de erro na view.
+                    // Formata mensagem de saída na view.
                     el('#feedback').innerHTML = `
                         <h2>Olá!</h2>
                         <p class="red">Algo deu errado e não foi possível enviar seu contato.</p>
-                        <p class="red">por favor, tente mais tarde.</p>
-                        <p><em>Obrigado!</em></p>
+                        <p class="red">Por favor, tente mais tarde.</p>
+                        <p><em>Obrigado...</em></p>
                     `;
+
+                    console.error('Falha ao enviar contato!');
 
                     // Se deu tudo certo...
                 } else {
 
-                    // Obtém só o primeiro nome do remetente.
+                    // Obtém o primeiro nome do remetente.
                     var name = contact.name.split(' ')[0];
 
-                    // Mensagem de saída para o usuário (feedback)
+                    // Formata mensagem de saída na view.
                     el('#feedback').innerHTML = `
-                        <h3>Olá ${name}!</h3>
+                        <h2>Olá ${name}!</h2>
                         <p>Seu contato foi enviado com sucesso.</p>
                         <p><em>Obrigado...</em></p>
                     `;
+
+                    console.log('Contato enviado com sucesso!');
                 }
+
             })
 
-            // Resposta do 'fetch' --> Falha ao enviar contato.
-            .catch(error => {
-                console.error(`Oooops! Algo deu muito errado: ${error}.`);
-            })
+        // Limpa os campos do formulário para permitir novos envios.
+        el('#contact-name').value = '';
+        el('#contact-email').value = '';
+        el('#contact-subject').value = '';
+        el('#contact-message').value = '';
 
-        // Limpa campos do formulário para permitir novos envios.
-        el('#contactName').value = '';
-        el('#contactEmail').value = '';
-        el('#contactSubject').value = '';
-        el('#contactMessage').value = '';
-
-        // Oculta o formulário.
+        // Oculta formulário.
         el('#contact').style.display = 'none';
 
-        // Mostra feedback
+        // Exibe mensagem de saída.
         el('#feedback').style.display = 'block';
 
         /**
          * Termina sem fazer mais nada.
          * Isso evita que o controle retorne para o HTML e que o formulário
-         * seja enviado por lá também, gerando um erro 404.
+         * seja enviado por lá também.
          */
         return false;
     }
@@ -145,34 +141,45 @@ if (typeof sendForm !== 'function') {
     console.log('Função "sendForm" já existe na memória. Não vou criar...');
 
 /**
- * Processa digitação nos campos.
- * 
- * A função 'inputFilters' evita que o usuário digite somente espaços no campo
+ * A função 'inputFilters' evita que o usuário digite somente espaços no campo 
  * do formulário. Também remove qualquer espaço duplicado digitado no campo.
  * 
- * Dica 1: esta função pode/deve ser ampliada com outros filtros que atuarão
- * durante a digitação (onkeyup) nos campos.
+ * Se o campo (HTML) tiver o atributo 'data-spaces="true"', a remoção dos 
+ * espaços duplicados é desligada para ele.
  * 
- * Dica 2: caso esta função seja necessária em outras páginas/formulários do
- * site, mova-a para './global.js'.
+ * Dica 1: esta função pode ser ampliada com outros filtros que atuarão durante a 
+ * digitação (onkeyup) nos campos.
+ * 
+ * Dica 2: caso esta função seja usada em outras páginas/formulários, mova-a 
+ * para ./global.js.
  * 
  *   Referências:
  *     https://www.w3schools.com/jsref/jsref_replace.asp
  *     https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/trimStart
  */
-if (typeof inputFilters !== 'function') {
+if (typeof inputFilters !== "function") {
+
+    console.log('Criando função "inputFilters"...');
+
     window.inputFilters = function () {
-        
+
         // Remove quaisquer espaços no começo do campo.
         this.value = this.value.trimStart();
 
-        // Remove espaços duplicados.
-        this.value = this.value.replace(/\s{2,}/g , ' ');
+        // Se 'data-spaces="true"' no campo, não faz mais nada
+        if (this.getAttribute('data-spaces') !== 'true')
+
+            /** 
+             * Para qualquer outro valor ou ausência de 'data-spaces', remove espaços duplicados do campo. 
+             */
+            this.value = this.value.replace(/\s{2,}/g, ' ');
     }
-}
+
+} else
+    console.log('Função "inputFilters" já existe na memória. Não vou criar...');
 
 /**
- * Processa o envio do formulário.
+ * Processa envio do formulário.
  *   Referências:
  *     https://www.w3schools.com/jsref/event_onsubmit.asp
  */
@@ -180,9 +187,8 @@ el('#contact').onsubmit = sendForm;
 
 /**
  * Processa cada campo do formulário ao ser preenchido.
- * Chama 'inputFilters' quando uma tecla é solta.
  */
 var inputs = el('#contact').elements;
-for (let i = 0; i < inputs.length; i++) {
+for (var i = 0; i < inputs.length; i++) {
     inputs[i].onkeyup = inputFilters;
 }
